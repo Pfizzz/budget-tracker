f880db4215afcd6e93c13242cb89e396cb70cccf
Pfizz <pfiste80@gmail.com>
2021-11-08 12:59:06 -0500

    added functional IndexedDB

--- public/js/index.js
+++ public/js/index.js
@@ -136,6 +136,8 @@ function sendTransaction(isAdding) {
   })
   .catch(err => {
     // fetch failed, so save in indexed db
+    console.log(err);
+    //ADDED THIS
     saveRecord(transaction);
 
     // clear form
